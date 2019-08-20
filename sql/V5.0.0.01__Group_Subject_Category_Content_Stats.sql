89bf33a04a14002309870ed8654c28a69092fe91
sheeban <sheeban@gooru.org>
2019-08-20 14:33:38 +0530

    Updated Schema based on the feedback

--- sql/V5.0.0.01__Group_Subject_Category_Content_Stats.sql
+++ sql/V5.0.0.01__Group_Subject_Category_Content_Stats.sql
@@ -1,25 +1,33 @@
-CREATE TABLE group_client_subject_data_reports
+CREATE TABLE group_client_subject_distribution_reports
 (
 	  id bigserial NOT NULL,
 	  tx_subject_code text NOT NULL,
 	  tx_sub_category_code text NOT NULL,
 	  total_count bigint NOT NULL DEFAULT 0,
-	  country_id bigint,
-	  month integer,
-	  year integer,
 	  tenant text,
 	  tenant_root text,
 	  partner text,
 	  created_at timestamp without time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
 	  updated_at timestamp without time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
-	  CONSTRAINT gcsdr_pkey PRIMARY KEY (id),
-	  CONSTRAINT gcsdr_country_id_fkey FOREIGN KEY (country_id)
-	      REFERENCES public.country (id) MATCH SIMPLE
-	      ON UPDATE NO ACTION ON DELETE NO ACTION
+	  CONSTRAINT gcsdr_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE group_client_content_distribution_reports
+(
+	  id bigserial NOT NULL,
+	  content_type text NOT NULL,
+	  total_count bigint NOT NULL DEFAULT 0,
+	  tenant text,
+	  tenant_root text,
+	  partner text,
+	  created_at timestamp without time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
+	  updated_at timestamp without time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
+	  CONSTRAINT gccdsr_pkey PRIMARY KEY (id),
+      CONSTRAINT gccdsr_content_type_check CHECK (((content_type)::text = ANY (ARRAY['assessment'::text, 'collection'::text, 'assessment-external'::text, 'question'::text, 'resource'::text, 'collection-external'::text, 'offline-activity'::text, 'course'::text])))
 );
 
 
-CREATE TABLE group_client_content_data_reports
+CREATE TABLE group_client_content_usage_reports
 (
 	  id bigserial NOT NULL,
 	  content_type text NOT NULL,
